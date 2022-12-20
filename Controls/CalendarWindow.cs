5f8f1008cfdc9d471e763b06649f60eafcf914a0
Yura <tarachom@gmail.com>
2022-12-20 20:04:53 +0200

    up

---
+++ Controls/CalendarWindow.cs
@@ -0,0 +1,51 @@
+using Gtk;
+
+namespace StorageAndTrade
+{
+    class CalendarWindow : Window
+    {
+        Calendar calendar;
+
+        public CalendarWindow() : base("Calendar")
+        {
+            SetDefaultSize(300, 0);
+            SetDefaultIconFromFile("images/form.ico");
+            SetPosition(WindowPosition.Mouse);
+            Modal = true;
+
+            VBox vbox = new VBox();
+            Add(vbox);
+
+            calendar = new Calendar();
+            calendar.DaySelectedDoubleClick += OnCalendarDaySelected;
+
+            vbox.PackStart(calendar, false, false, 0);
+
+            ShowAll();
+        }
+
+        public Action<DateTime>? Select { get; set; }
+
+        DateTime mValue;
+        public DateTime Value
+        {
+            get
+            {
+                return mValue;
+            }
+            set
+            {
+                mValue = value;
+                calendar.Date = value;
+            }
+        }
+
+        void OnCalendarDaySelected(object? sender, EventArgs args)
+        {
+            if (Select != null)
+                Select.Invoke(new DateTime(calendar.Date.Year, calendar.Date.Month, calendar.Date.Day, Value.Hour, Value.Minute, Value.Second));
+
+            Close();
+        }
+    }
+}
--- Controls/DateTimeControl.cs
+++ Controls/DateTimeControl.cs
@@ -8,8 +8,19 @@ namespace StorageAndTrade
 
         public DateTimeControl() : base()
         {
+            //Entry
+            entryDateTimeValue.Changed += OnEntryDateTimeChanged;
+
             PackStart(entryDateTimeValue, false, false, 2);
+
+            //Button
+            Button openCalendarButton = new Button(new Image("images/find.png"));
+            openCalendarButton.Clicked += OnOpenCalendar;
+
+            PackStart(openCalendarButton, false, false, 2);
         }
+
+        bool disableChanged = false;
 
         DateTime mValue;
         public DateTime Value
@@ -21,7 +32,35 @@ namespace StorageAndTrade
             set
             {
                 mValue = value;
+
+                disableChanged = true;
                 entryDateTimeValue.Text = mValue.ToString();
+                disableChanged = false;
+            }
+        }
+
+        void OnOpenCalendar(object? sender, EventArgs args)
+        {
+            CalendarWindow cw = new CalendarWindow();
+            cw.Select = (DateTime x) => { Value = x; };
+            cw.Value = Value;
+            cw.Show();
+        }
+
+        void OnEntryDateTimeChanged(object? sender, EventArgs args)
+        {
+            if (!disableChanged)
+            {
+                DateTime value;
+                if (DateTime.TryParse(entryDateTimeValue.Text, out value))
+                {
+                    mValue = value;
+                    Console.WriteLine(mValue);
+                }
+                else
+                {
+                    //
+                }
             }
         }
     }
