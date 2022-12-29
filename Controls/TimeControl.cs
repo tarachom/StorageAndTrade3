93c1b511d27a3c61f3d92bf84af1dd54164ff670
Yura <tarachom@gmail.com>
2022-12-29 23:43:00 +0200

    up

--- Controls/TimeControl.cs
+++ Controls/TimeControl.cs
@@ -5,9 +5,13 @@ namespace StorageAndTrade
     class TimeControl : HBox
     {
         Entry entryTime = new Entry();
+        HBox hBoxInfoValid = new HBox() { WidthRequest = 16 };
 
         public TimeControl() : base()
         {
+            PackStart(hBoxInfoValid, false, false, 1);
+
+            entryTime.Changed += OnEntryTimeChanged;
             PackStart(entryTime, false, false, 2);
         }
 
@@ -23,6 +27,40 @@ namespace StorageAndTrade
                 mValue = value;
                 entryTime.Text = mValue.ToString();
             }
+        }
+
+        void ClearHBoxInfoValid()
+        {
+            foreach (Widget item in hBoxInfoValid.Children)
+                hBoxInfoValid.Remove(item);
+        }
+
+        public bool IsValidValue()
+        {
+            ClearHBoxInfoValid();
+
+            TimeSpan value;
+            if (TimeSpan.TryParse(entryTime.Text, out value))
+            {
+                mValue = value;
+
+                hBoxInfoValid.Add(new Image("images/16/ok.png"));
+                hBoxInfoValid.ShowAll();
+
+                return true;
+            }
+            else
+            {
+                hBoxInfoValid.Add(new Image("images/16/error.png"));
+                hBoxInfoValid.ShowAll();
+
+                return false;
+            }
+        }
+
+        void OnEntryTimeChanged(object? sender, EventArgs args)
+        {
+            IsValidValue();
         }
     }
 }
