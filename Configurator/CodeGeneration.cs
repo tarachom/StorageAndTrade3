2c6882db31d49d00b70a717a2b04b6085f75730e
Yura <tarachom@gmail.com>
2023-01-05 13:24:06 +0200

    up

--- Configurator/CodeGeneration.cs
+++ Configurator/CodeGeneration.cs
@@ -1,6 +1,6 @@
 ﻿
 /*
-Copyright (C) 2019-2022 TARAKHOMYN YURIY IVANOVYCH
+Copyright (C) 2019-2023 TARAKHOMYN YURIY IVANOVYCH
 All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
