443a1361cfba7901d6a2d4b62ce25a010d3f0bdf
Yura <tarachom@gmail.com>
2023-02-07 16:33:37 +0200

    up

--- Довідники/ДоговориКонтрагентів/ДоговориКонтрагентів_PointerControl.cs
+++ Довідники/ДоговориКонтрагентів/ДоговориКонтрагентів_PointerControl.cs
@@ -1,5 +1,3 @@
-#region Info
-
 /*
 Copyright (C) 2019-2023 TARAKHOMYN YURIY IVANOVYCH
 All rights reserved.
@@ -22,8 +20,6 @@ limitations under the License.
 Адреса:   Україна, м. Львів
 Сайт:     accounting.org.ua
 */
-
-#endregion
 
 using StorageAndTrade_1_0.Довідники;
 
